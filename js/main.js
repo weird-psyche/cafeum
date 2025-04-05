538c25b556fce689ab36a04eacb0048ea736d9d4
weird-psyche <92737045+weird-psyche@users.noreply.github.com>
2025-04-05 18:31:02 +0200

    update 100%

--- js/main.js
+++ js/main.js
@@ -24,35 +24,6 @@
             $('.sticky-top').removeClass('shadow-sm').css('top', '-150px');
         }
     });
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
-
-
-    // Modal Video
-    var $videoSrc;
-    $('.btn-play').click(function () {
-        $videoSrc = $(this).data("src");
-    });
-    console.log($videoSrc);
-    $('#videoModal').on('shown.bs.modal', function (e) {
-        $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
-    })
-    $('#videoModal').on('hide.bs.modal', function (e) {
-        $("#video").attr('src', $videoSrc);
-    })
-
 
     // Product carousel
     $(".product-carousel").owlCarousel({
