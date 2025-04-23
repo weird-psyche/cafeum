935441fe61ded1593b4b1f4fc0853318743c864f
weird-psyche <92737045+weird-psyche@users.noreply.github.com>
2025-04-23 15:52:06 +0200

    Add files via upload

---
+++ js/contact.js
@@ -0,0 +1,32 @@
+function KontaktKuldes() {
+  const form = document.getElementById("ContactForm");
+  const formData = {
+    TeljesNev: form.name.value,
+    Email: form.email.value,
+    Tema: form.subject.value,
+    Uzenet: form.message.value,
+  };
+
+  //console.log(JSON.stringify(formData));
+
+  const xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function(){
+    if(xmlhttp.readyState==4 && xmlhttp.status==200){
+      console.log("Sikeres POST keres!");
+    } else if(xmlhttp.readyState==4){
+      console.log("Hiba a POST keres soran: ",xmlhttp.status);
+    }
+  };
+
+// lokalis szerver:
+//xmlhttp.open("POST","https://localhost:7032/api/cafeum/kontakt",true);
+//xmlhttp.setRequestHeader("Content-Type", "application/json");
+
+// online szerver:
+xmlhttp.open("POST","https://api-20nv4395y20947yv5n2y4v02y3v20n43v5273vy-ngrok-dev.eu.ngrok.io/api/cafeum/kontakt",true);
+xmlhttp.setRequestHeader("Content-Type", "application/json");
+xmlhttp.setRequestHeader("ngrok-skip-browser-warning", "1");
+
+xmlhttp.send(JSON.stringify(formData));
+alert("Sikeres küldés!");
+}
---
+++ js/register.js
@@ -0,0 +1,42 @@
+function AccountCreate() {
+
+  let Pwd1 = document.getElementById('Password').value;
+  let Pwd2 = document.getElementById('Password2').value;
+  let Lakcim = document.getElementById('Location').value;
+
+  if(Pwd1 != Pwd2) document.getElementById('BadPwd').innerHTML="ELTERO JELSZO";
+  else{
+  const logindiv = document.getElementById("LoginForm");
+  const DivData = {
+    //vasarloId: getId,
+    vasarloNev: logindiv.VezNev.value+" "+logindiv.KerNev.value,
+    vasarloMail: logindiv.Email.value,
+    vasarloJelszo: logindiv.Password.value,
+    //vasarloCim: logindiv.Location.value,
+    vasarloCim: Lakcim.toString(),
+  };
+  console.log(JSON.stringify(DivData));
+  const xmlhttp = new XMLHttpRequest();
+
+  xmlhttp.onreadystatechange = function(){
+  if(xmlhttp.readyState==4 && xmlhttp.status==200){
+    console.log("Sikeres POST keres!");
+  } else if(xmlhttp.readyState==4){
+    console.log("Hiba a POST keres soran: "+xmlhttp.status);
+  }
+};
+// lokalis szerver:
+//xmlhttp.open("POST","https://localhost:7032/api/cafeum/vasarlok",true);
+//xmlhttp.setRequestHeader("Content-Type", "application/json");
+
+// online szerver:
+xmlhttp.open("POST","https://api-20nv4395y20947yv5n2y4v02y3v20n43v5273vy-ngrok-dev.eu.ngrok.io/api/cafeum/vasarlok",true);
+xmlhttp.setRequestHeader("Content-Type", "application/json");
+xmlhttp.setRequestHeader("ngrok-skip-browser-warning", "1");
+
+xmlhttp.send(JSON.stringify(DivData));
+
+alert("Sikeres regisztracio");
+location.assign("login.html");
+}
+}
